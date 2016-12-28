35f19cccd2ad9f34db4145d16c925eb5c0bd5fcf
Villy Siu <villysiu@gmail.com>
2016-12-28 19:05:59 +0000

    Done.

---
+++ script.js
@@ -0,0 +1,10 @@
+// We should wait for the page to load before running our Ajax request
+$(document).ready(function(){
+  // Now we start the Ajax GET request. The first parameter is the URL with the data.
+  // The second parameter is a function that handles the response.
+  $.get("sentence.html", function(response) {
+    // Here we are getting the element on the page with the id of sentences and
+    // inserting the response
+    $("#sentences").html(response);
+  });
+});
